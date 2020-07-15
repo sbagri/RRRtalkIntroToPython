51ec68458e1ae7c8b8a9e553093f01c995d2c339
sbagri <sbagri@vt.edu>
2020-07-15 00:19:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# All programing langauages require some setup. However, we are short on time today and everyone may have a different setup, we will use online solutions which will work well for a short session like this. As you progress, you should consider installing all the requirements for python
+
+# We will use python 3, specifially python3.8 which is the latest
+# Discussion of python 2 and 3
+
+# One of the functions is print. Use to print something on terminal
+# What is terminal?
+
+
+# what are comments
+
+# Any programming language reads, executes, prints/changes state etc. and repeats this cycle again. Good mental model of how things are working
+
+print("Hello World")
